197f5a8dc401ea171376725a39c0782080764419
Kris1027 <obarzanek.work@gmail.com>
2023-11-11 13:11:20 +0100

    fix bug video of home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Wrapper from '../components/Wrapper';
 export default function Home() {
   return (
     <Wrapper>
-      <video autoPlay muted src='/home.mp4'></video>
+      <video autoPlay muted playsInline src='/home.mp4'></video>
     </Wrapper>
   );
 }
