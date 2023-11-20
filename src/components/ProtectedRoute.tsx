0f57bf6b1c6ddbfc98a70f398b137ac886482a6f
Kris1027 <obarzanek.work@gmail.com>
2023-11-20 09:44:20 +0100

    after login admin-dashboard

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,20 +1,9 @@
-import { useNavigate } from 'react-router-dom';
 import { useUser } from '../authentication/useUser';
 import AdminDashboard from '../pages/AdminDashboard';
 import Spinner from './Spinner';
-import { useEffect } from 'react';
 
 export default function ProtectedRoute() {
-  const navigate = useNavigate();
-
   const { isLoading, isAuthenicated } = useUser();
-
-  useEffect(
-    function () {
-      if (!isAuthenicated && !isLoading) navigate('/');
-    },
-    [isAuthenicated, isLoading, navigate]
-  );
 
   if (isLoading) return <Spinner />;
 
