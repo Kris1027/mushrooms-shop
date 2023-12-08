1ab4d913cb5d56dbc5e6bb4c68676c3ba656238a
Kris1027 <obarzanek.work@gmail.com>
2023-12-08 23:45:04 +0100

    add .env file and save passwords

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -4,14 +4,47 @@ import Button from './Button';
 export default function Summary() {
   const cartItems = useCartSelector((state) => state.cart.items);
 
+  const orderedProducts = cartItems
+    .map((item) => `${item.name}, ${item.form} - x${item.quantity}`)
+    .join('\n');
+
   const totalPrice = cartItems.reduce(
     (val, item) => val + (item.regularPrice - item.discount) * item.quantity,
     0
   );
 
   const formattedTotalPrice = totalPrice.toFixed(2);
-
   const deliveryCost = 20;
+  const orderNr = Math.floor(Math.random() * 10000000);
+  const finalPrice = totalPrice + deliveryCost;
+
+  const handleBuyMail = () => {
+    const subject = encodeURIComponent(`Zamównienie nr: ${orderNr}`);
+    const body = encodeURIComponent(
+      `${orderedProducts}
+      \nDo zapłaty: ${finalPrice} zł
+      \n DANE DO WYSYŁKI: (proszę wypełnić)
+      \n Imię i nazwisko: 
+      \n Ulica i nr domu/mieszkania: 
+      \n Kod pocztowy: 
+      \n Miasto: 
+      \n
+      \n DANE DO PRZELEWU:
+      \n ${import.meta.env.VITE_NAME}
+      \n nr konta: ${import.meta.env.VITE_BANK_ACCOUNT}
+      \n blik: ${import.meta.env.VITE_BLIK}
+      \n
+      \n 
+      \n W tytule przelewu proszę podać numer zamówienia który jest podany w tytule tego maila!
+      \n Po udanej wpłacie wyślemy potwierdzenie zamówienia`
+    );
+
+    const mailTo = `mailto:${
+      import.meta.env.VITE_EMAIL_ADRESS
+    }?subject=${subject}&body=${body}`;
+
+    window.location.href = mailTo;
+  };
 
   return (
     <div>
@@ -25,8 +58,8 @@ export default function Summary() {
       ))}
       <p>Produkty: {formattedTotalPrice} zł</p>
       <p>Przesyłka: {deliveryCost} zł</p>
-      <p>Razem: {totalPrice + deliveryCost} zł</p>
-      <Button>Kupuję!</Button>
+      <p>Razem: {finalPrice} zł</p>
+      <Button onClick={handleBuyMail}>Kupuję!</Button>
     </div>
   );
 }
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://upysehsmhgosygexmqoz.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVweXNlaHNtaGdvc3lnZXhtcW96Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTc4MjA3NywiZXhwIjoyMDE1MzU4MDc3fQ.3M3WVfe__MXCVOkP7sM0yti7al4zTkdQUWtGAjg8LLk';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
